bb58268b7b64a341f352a84b2ae7b3d375e02f04
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-25 15:20:55 -0500

    adds bio

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,7 +5,19 @@ export default function Profile(props) {
   return(
     <div className='Profile'>
       <img className='Profile-picture' alt='Profile' width="300" src='../profile_picture.png' />
-      <div className='Profile-name-container'><h1 className='Profile-name'>Solomon Raphael Feldman</h1></div>
+      <div className='Profile-name-container'>
+        <h1 className='Profile-name'>Solomon Raphael Feldman</h1>
+        <hr className='Profile-horizontal-rule'/>
+        <p className='Profile-bio'>
+          Full Stack Web Developer with a passion for developer friendly code and user friendly websites. 
+          With a background in studying Physics and Computer Science, and an interest in Music, 
+          he began his journey into the world of web development through desiring a more practical application for his education. 
+          <br/><br/>
+          Throughout his web development experience he's learned to value writing readable, maintainable, and testable code, 
+          not only for the sake of others, but for his own ability to maintain and add on to his own code.
+        </p>
+        <hr className='Profile-horizontal-rule'/>
+      </div>
     </div>
   )
 }
