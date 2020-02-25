534300864044b467487ab95d9f178540f26edede
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-25 14:42:52 -0500

    positions name

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,8 +4,8 @@ import './Profile.css'
 export default function Profile(props) {
   return(
     <div className='Profile'>
-      <img alt='beep' height="300" width="300" src='../profile_picture.png' />
-      <h1 className='Profile-name'>Solomon Raphael Feldman</h1>
+      <img className='Profile-picture' alt='beep' width="300" src='../profile_picture.png' />
+      <div className='Profile-name-container'><h1 className='Profile-name'>Solomon Raphael Feldman</h1></div>
     </div>
   )
 }
