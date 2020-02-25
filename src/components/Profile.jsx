e7bbe2412825f083dc7e6123309b297fac0fdcb7
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-25 14:52:30 -0500

    adds profile picture alt

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,7 +4,7 @@ import './Profile.css'
 export default function Profile(props) {
   return(
     <div className='Profile'>
-      <img className='Profile-picture' alt='beep' width="300" src='../profile_picture.png' />
+      <img className='Profile-picture' alt='Profile' width="300" src='../profile_picture.png' />
       <div className='Profile-name-container'><h1 className='Profile-name'>Solomon Raphael Feldman</h1></div>
     </div>
   )
