b7f4a1b5a2ff876f3afecbadd77fd8839f6e92c0
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-03-02 13:47:59 -0500

    adds project content constant

---
+++ src/constants/Projects.js
@@ -0,0 +1,56 @@
+const DOMAIN_NAME = 'solomonfeldman.dev'
+const GIT_HUB = 'https://github.com/SolomonRFeldman/'
+
+const GOOD_WRITES = {
+  id: 1,
+  name: 'Good Writes',
+  repo: `${GIT_HUB}good-writes`,
+  website: `https://goodwrites.${DOMAIN_NAME}`,
+  description: 'Writers workshop Rails/React App where users can share and workshop their writing',
+  bullets: [
+    "Developed a Rails API backend with endpoints for users, groups, and pieces",
+    "Enabled alternative login through Github OAuth",
+    "Implemented React / Redux frontend user interface with bootstrap styling",
+    "Utilized JSON web tokens and localStorage to encrypt and store user information",
+    "Converted full Rails front/back end app to Rails/React app",
+    "Utilized Test Driven Development with RSpec for backend"
+  ]
+}
+
+const CK2_CHARACTER_BUILDER = {
+  id: 2,
+  name: 'CKII Character Builder',
+  repo: `${GIT_HUB}ck2-character-builder`,
+  website: `https://ck2characterbuilder.${DOMAIN_NAME}`,
+  description: 'Character Builder/Planner for persisting Crusader Kings 2 custom character builds',
+  bullets: [
+    "Utilized ES6 Vanilla Javascript syntax to program frontend interactions",
+    "Converted Vanilla Javascript front end to React",
+    "Created a docker container out of both the front and backend",
+    "Stylized and scripted user interface using bootstrap, bootstrap-native, and react-bootstrap",
+    "Built Rails API backend to fetch and store character data",
+    "Utilized Jest and React-Testing-Library to test the frontend",
+    "Containerized using docker-compose",
+    "Directed traffic to the right port using Haproxy on the website’s server",
+    "Built UI and controls to be compatible with mobile"
+  ]
+}
+
+const STOCKY = {
+  id: 3,
+  name: 'Stocky',
+  repo: `${GIT_HUB}stocky`,
+  website: `https://stocky.${DOMAIN_NAME}`,
+  description: 'Rails/React app for simulation stock buying/selling',
+  bullets: [
+    'Utilized 3rd party api to provide up to date stock information'
+  ]
+}
+
+const PROJECTS = [
+  GOOD_WRITES,
+  CK2_CHARACTER_BUILDER,
+  STOCKY
+]
+
+export default PROJECTS
