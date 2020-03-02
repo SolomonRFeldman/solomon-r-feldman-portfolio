586c5e80500855f7afc7eb5c069d7f92ffe25884
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-03-01 23:09:33 -0500

    adds basic project info container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Profile from './components/Profile';
+import Projects from './components/Project/Projects';
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Project/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Project from './Project'
+import './Projects.css'
+
+export default function Projects(props) {
+  return(
+    <div className='Projects'>
+      <Project />
+    </div>
+  )
+}
