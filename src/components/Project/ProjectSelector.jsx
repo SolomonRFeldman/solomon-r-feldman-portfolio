a224b382b6d767c7ba534b9b9c16b2bcc449b45a
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-03-02 12:50:32 -0500

    adds selectable tabs

--- src/components/Project/ProjectSelector.jsx
+++ src/components/Project/ProjectSelector.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProjectSelector.css'
 
 export default function ProjectSelector(props) {
+  const [selected, setSelected] = useState('0')
+
   return(
     <div className='ProjectSelector'>
-      <div className='ProjectSelector-tab selected'><span>Good Writes</span></div>
-      <div className='ProjectSelector-tab'><span>CKII Character Builder</span></div>
-      <div className='ProjectSelector-tab'><span>Stocky</span></div>
+      <ProjectTab id='0' selected={selected} setSelected={setSelected}>Good Writes</ProjectTab>
+      <ProjectTab id='1' selected={selected} setSelected={setSelected}>CKII Character Builder</ProjectTab>
+      <ProjectTab id='2' selected={selected} setSelected={setSelected}>Stocky</ProjectTab>
+    </div>
+  )
+}
+
+function ProjectTab({id, children, selected, setSelected}) {
+  const isSelected = () => {
+    return selected === id ? true : false
+  }
+  const handleClick = () => setSelected(id)
+
+  return(
+    <div key={id} className={`ProjectSelector-tab ${isSelected() ? 'selected' : null}`} onClick={handleClick}>
+      <span>{children}</span>
     </div>
   )
 }
