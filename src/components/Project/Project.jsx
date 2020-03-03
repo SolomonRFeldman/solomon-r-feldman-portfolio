9174540eadddd6592170b4bb72718fc7c9fcf5e0
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-03-03 09:58:37 -0500

    adds project descriptions

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -4,7 +4,16 @@ import './Project.css'
 export default function Project({project}) {
   return(
     <div className='Project'>
-      <h1>{project.name}</h1>
+      <div className='Project-background'>
+        <img height='600px' src={project.background}></img>
+      </div>
+      <div className='Project-body'>
+        <h1 className='Project-name'>{project.name}</h1>
+        <p className='Project-text'>
+          {project.description}
+          <ul className='Project-bullet-list'>{project.bullets.map(bullet => <li className='Project-bullet'>{bullet}</li>)}</ul>
+        </p>
+      </div>
     </div>
   )
 }
