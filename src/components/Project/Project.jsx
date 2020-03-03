865a54b5910fe8130c37029edbe0b6fd4b53c063
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-03-03 10:37:39 -0500

    adds project links

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -8,11 +8,22 @@ export default function Project({project}) {
         <img height='600px' src={project.background}></img>
       </div>
       <div className='Project-body'>
-        <h1 className='Project-name'>{project.name}</h1>
-        <p className='Project-text'>
-          {project.description}
-          <ul className='Project-bullet-list'>{project.bullets.map(bullet => <li className='Project-bullet'>{bullet}</li>)}</ul>
-        </p>
+        <div style={{display: 'inline-block'}} className='Project-description'>
+          <h1 className='Project-name'>{project.name}</h1>
+          <p className='Project-text'>
+            {project.description}
+            <ul className='Project-bullet-list'>{project.bullets.map(bullet => <li className='Project-bullet'>{bullet}</li>)}</ul>
+          </p>
+        </div>
+        <div className='Project-contact'>
+          <a className='Project-link' href={project.website} target="_blank" rel="noopener noreferrer">
+            <span className='Project-link-text'>Project Website</span>
+          </a>
+          <a className='Project-link' href={project.repo} target="_blank" rel="noopener noreferrer">
+            <img alt='GitHub Mark' height="24" src='../GitHub-Mark-Light-32px.png' />
+            <span className='Project-github-link-text'>Project Repo</span>
+          </a>
+        </div>
       </div>
     </div>
   )
